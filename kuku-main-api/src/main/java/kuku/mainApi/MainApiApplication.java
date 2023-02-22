53da0bff02971d8972fc6d58a2d14977a6d5c957
박종빈 <runnz121@gmail.com>
2023-02-23 00:00:17 +0900

    main-api, rds querydsl gradle 파일 설정

---
+++ kuku-main-api/src/main/java/kuku/mainApi/MainApiApplication.java
@@ -0,0 +1,12 @@
+package kuku.mainApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApiApplication.class, args);
+    }
+}
