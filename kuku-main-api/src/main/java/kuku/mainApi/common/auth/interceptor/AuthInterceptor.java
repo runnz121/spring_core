49d4c1f0576d6607543102b943265fd9ffd44439
박종빈 <runnz121@gmail.com>
2023-03-24 23:45:33 +0900

    interceptor jwt token 설정

---
+++ kuku-main-api/src/main/java/kuku/mainApi/common/auth/interceptor/AuthInterceptor.java
@@ -0,0 +1,43 @@
+package kuku.mainApi.common.auth.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+
+        String jwtAuthToken = request.getHeader("Authorization");
+        boolean isActiveToken = checkJwtToken(jwtAuthToken);
+
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    private boolean checkJwtToken(String authToken) {
+
+
+    }
+
+    private
+
+
+
+
+
+}
