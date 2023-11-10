07e6f56dccbd85de8105fdc086827ad4acfef50b
박종빈 <runnz121@gmail.com>
2023-11-10 21:00:01 +0900

    [feature] custom executorpool 생성

--- kuku-main-api/src/main/java/kuku/mainApi/MainApiApplication.java
+++ kuku-main-api/src/main/java/kuku/mainApi/MainApiApplication.java
@@ -3,9 +3,7 @@ package kuku.mainApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 @EnableJpaAuditing
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MainApiApplication {
---
+++ kuku-main-api/src/main/java/kuku/mainApi/common/config/async/AsyncConfiguration.java
@@ -0,0 +1,40 @@
+package kuku.mainApi.common.config.async;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.Executor;
+
+@Slf4j
+@EnableAsync
+@RequiredArgsConstructor
+public class AsyncConfiguration implements AsyncConfigurer {
+    
+    private final TaskExecutionProperties taskExecutionProperties;
+    
+    @Override
+    @Bean(name = "taskExecutor")
+    public Executor getAsyncExecutor() {
+        log.info("custom AsyncConfiguration");
+        
+        return getAsyncExecutor(
+                taskExecutionProperties.getPool(),
+                taskExecutionProperties.getThreadNamePrefix()
+        );
+    }
+
+    private Executor getExecutor(TaskExecutionProperties.Pool pool, String threadNamePrefix) {
+        ContextAwarePoolExecutor executor = new ContextAwarePoolExecutor();
+
+        executor.setCorePoolSize(pool.getCoreSize());
+        executor.setMaxPoolSize(pool.getMaxSize());
+        executor.setQueueCapacity(pool.getQueueCapacity());
+        executor.setThreadNamePrefix(threadNamePrefix);
+
+        return executor;
+    }
+}
