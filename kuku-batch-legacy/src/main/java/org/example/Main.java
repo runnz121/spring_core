bb6e7bd615e5db97e04255656107a4683b3ffd82
박종빈 <runnz121@gmail.com>
2024-04-04 23:17:39 +0900

    [feat] 배치 설정

--- kuku-batch-legacy/src/main/java/org/example/Main.java
+++ kuku-batch-legacy/src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableBatchProcessing
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
