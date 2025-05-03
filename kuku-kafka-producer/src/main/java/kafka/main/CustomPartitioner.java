6ebc32f79783cc49712a84a423d264f42499dc4e
runnz121 <runnz121@gmail.com>
2025-05-04 03:28:07 +0900

    [feat] custom partitioner 구현

---
+++ kuku-kafka-producer/src/main/java/kafka/main/CustomPartitioner.java
@@ -0,0 +1,73 @@
+package kafka.main;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.clients.producer.internals.DefaultPartitioner;
+import org.apache.kafka.clients.producer.internals.StickyPartitionCache;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.InvalidRecordException;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+
+public class CustomPartitioner implements Partitioner {
+
+//    DefaultPartitioner
+
+    public static final Logger logger = LoggerFactory.getLogger(CustomPartitioner.class.getName());
+
+    private String specialKeyName;
+
+    private final StickyPartitionCache stickyPartitionCache = new StickyPartitionCache();
+
+    @Override
+    public int partition(String topic,
+                         Object key,
+                         byte[] keyBytes,
+                         Object value,
+                         byte[] valueBytes,
+                         Cluster cluster) {
+
+        // 파티션 정보들이 담겨있음
+        List<PartitionInfo> partitionInfos = cluster.partitionsForTopic(topic);
+        int numPartitions = partitionInfos.size();
+        int numSpecialPartitions = (int) (numPartitions * 0.5);
+        int partitionIndex = 0;
+
+        if (keyBytes == null) {
+            // 스티키로 하던지 혹은 에러로 하던지
+//            return stickyPartitionCache.partition(topic, cluster);
+            throw new InvalidRecordException("key should not be null");
+        }
+
+        // 0, 1 번 파티션에만 P001 키가 전송된다
+        if (((String)key).equals(specialKeyName)) {
+            // DefaultPartition 기본 반환 값
+            partitionIndex = Utils.toPositive(Utils.murmur2(valueBytes)) % numSpecialPartitions;
+        }
+
+        else {
+            // 0, 1 제외
+            partitionIndex = Utils.toPositive(Utils.murmur2(keyBytes)) % (numPartitions - numSpecialPartitions) + numSpecialPartitions;
+        }
+
+        logger.info("key : {} is sent to partition : {}", key, partitionIndex);
+
+        return partitionIndex;
+    }
+
+    // 파티셔너 종료될 때
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+        specialKeyName = configs.get("custom.specialKey").toString();
+    }
+}
---
+++ kuku-kafka-producer/src/main/java/kafka/main/PizzaProducerCustomPartitioner.java
@@ -0,0 +1,131 @@
+package kafka.main;
+
+import com.github.javafaker.Faker;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+
+public class PizzaProducerCustomPartitioner {
+    public static final Logger logger = LoggerFactory.getLogger(PizzaProducerCustomPartitioner.class.getName());
+
+    // 메시지 보낼 메서드
+    public static void sendPizzaMessage(KafkaProducer<String, String> kafkaProducer,
+                                        String topicName, int iterCount,
+                                        int interIntervalMillis, int intervalMillis,
+                                        int intervalCount, boolean sync) {
+
+        PizzaMessage pizzaMessage = new PizzaMessage();
+        int iterSeq = 0;
+        long seed = 2022;
+        Random random = new Random(seed);
+        Faker faker = Faker.instance(random);
+
+        long startTime = System.currentTimeMillis();
+
+        while( iterSeq++ != iterCount ) {
+
+            HashMap<String, String> pMessage = pizzaMessage.produce_msg(faker, random, iterSeq);
+
+            // record 생성
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, pMessage.get("key"), pMessage.get("message"));
+
+            // 메세지 송출
+            sendMessage(kafkaProducer, producerRecord, pMessage, sync);
+
+            if ((intervalCount > 0) && (iterSeq % intervalCount == 0)) {
+                try {
+                    logger.info("####### IntervalCount:" + intervalCount + " intervalMillis:" + intervalMillis + " #########");
+                    Thread.sleep(intervalMillis);
+                } catch (InterruptedException e) {
+                    logger.error(e.getMessage());
+                }
+            }
+
+            if (interIntervalMillis > 0) {
+                try {
+                    logger.info("interIntervalMillis:" + interIntervalMillis);
+                    Thread.sleep(interIntervalMillis);
+                } catch (InterruptedException e) {
+                    logger.error(e.getMessage());
+                }
+            }
+
+        }
+        long endTime = System.currentTimeMillis();
+        long timeElapsed = endTime - startTime;
+
+        logger.info("{} millisecond elapsed for {} iterations", timeElapsed, iterCount);
+
+    }
+
+    public static void sendMessage(KafkaProducer<String, String> kafkaProducer,
+                                   ProducerRecord<String, String> producerRecord,
+                                   HashMap<String, String> pMessage, boolean sync) {
+        // 비동기 방식으로 처리
+        if (!sync) {
+            kafkaProducer.send(producerRecord, (metadata, exception) -> {
+
+                if (exception == null) {
+                    logger.info("async message:" + pMessage.get("key") + " partition:" + metadata.partition() + " offset:" + metadata.offset());
+                } else {
+                    logger.error("exception error from broker " + exception.getMessage());
+                }
+            });
+        }
+
+        // 동기 방식으로 처리
+        else {
+            try {
+
+                RecordMetadata metadata = kafkaProducer.send(producerRecord).get(); // .get() 방식은 동기방식이다.
+                logger.info("sync message:" + pMessage.get("key") + " partition:" + metadata.partition() + " offset:" + metadata.offset());
+
+            } catch (ExecutionException e) {
+                logger.error(e.getMessage());
+            } catch (InterruptedException e) {
+                logger.error(e.getMessage());
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        String topicName = "pizza-topic-partitioner";
+        //KafkaProducer configuration setting
+        // null, "hello world"
+
+        Properties props  = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:10000");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+//        props.setProperty("partitioner.class", "CustomPartitioner");
+
+        // CustomPartitioner.class 를 참조
+        props.setProperty(ProducerConfig.PARTITIONER_CLASS_CONFIG, "kafka.main.CustomPartitioner");
+        props.setProperty("custom.specialKey", "P001");
+
+        //KafkaProducer object creation
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(props);
+
+        sendPizzaMessage(kafkaProducer,
+                topicName,
+                -1,
+                1000,
+                0,
+                0,
+                true); // 메시지볼려고 true로 바꿈
+
+        kafkaProducer.close();
+
+    }
+}
